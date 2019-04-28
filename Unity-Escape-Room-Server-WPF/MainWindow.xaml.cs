deaeac7e0798bb844952290b00c9425d324f2b54
Fire Banana <free0run@gmail.com>
2019-04-28 20:23:31 +0500

    Change to WPF

---
+++ Unity-Escape-Room-Server-WPF/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Unity_Escape_Room_Server_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NetworkHandler handler;
+        Dictionary<TcpClient, int> TeamListDictionary = new Dictionary<TcpClient, int>();
+
+        void AddToList(string name, TcpClient client)
+        {
+            var pos = ClientListBox.Items.Add(name);
+            TeamListDictionary.Add(client, pos);
+        }
+
+        void RemoveFromList(TcpClient client)
+        {
+            if(TeamListDictionary.ContainsKey(client))
+            {
+                ClientListBox.Items.RemoveAt(TeamListDictionary[client]);
+                TeamListDictionary.Remove(client);
+            }
+            else
+            {
+                MessageBox.Show("Team is not in list but trying to be removed");
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            handler = new NetworkHandler("localhost");
+            IpAddressText.Content = handler.IpAddress + ":" + NetworkHandler.PORT;
+
+            handler.OnClientConnected += (teamName, client) =>
+            {
+                if (ClientListBox.Dispatcher.Thread != Thread.CurrentThread)
+                {
+                    var del = new NetworkHandler.ClientConnectionCallback((newTeamName, newClient) =>
+                    {
+                        AddToList(newTeamName, newClient);
+                    });
+                    ClientListBox.Dispatcher.BeginInvoke(del, new object[] { teamName });
+                }
+                else
+                {
+                    AddToList(teamName, client);
+                }
+            };
+            handler.OnClientDisconnected += (client) =>
+            {
+                if (ClientListBox.Dispatcher.Thread != Thread.CurrentThread)
+                {
+                    var del = new NetworkHandler.ClientDisconnectionCallback(newClient =>
+                    {
+                        RemoveFromList(newClient);
+                    });
+                    ClientListBox.Dispatcher.BeginInvoke(del, new object[] { client });
+                }
+                else
+                {
+                    RemoveFromList(client);
+                }
+            };
+        }
+    }
+}
---
+++ Unity-Escape-Room-Server-WPF/NetworkHandler.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Unity_Escape_Room_Server_WPF
+{
+    class NetworkHandler
+    {
+        public const int PORT = 2000;
+        TcpListener listener;
+
+        public List<TcpClient> ClientList = new List<TcpClient>();
+        public string IpAddress;
+
+        //Events
+        public delegate void ClientConnectionCallback(string teamName, TcpClient client);
+        public delegate void ClientDisconnectionCallback(TcpClient client);
+        public event ClientConnectionCallback OnClientConnected;
+        public event ClientDisconnectionCallback OnClientDisconnected;
+
+        public NetworkHandler(string ipAddress)
+        {
+            foreach (var address in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    listener = new TcpListener(address, PORT);
+                    IpAddress = address.ToString();
+                }
+            }
+
+            listener.Start();
+            BeginExecutingClientConnectListener();
+        }
+
+        public void BeginExecutingClientConnectListener()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var client = listener.AcceptTcpClient();
+                    OnClientConnect(client);
+                }
+            });
+        }
+
+        void OnClientConnect(TcpClient client)
+        {
+            ClientList.Add(client);
+            BeginClientListener(client);            
+        }
+
+        void OnClientDisconnect(TcpClient client)
+        {
+            OnClientDisconnected.Invoke(client);
+        }
+
+        void SendAuthenticationResponse(TcpClient client, string teamName)
+        {
+            var packet = new AuthenticationResponsePacket(teamName);
+
+            var serializedPacket = JsonConvert.SerializeObject(packet);
+            var buff = Encoding.ASCII.GetBytes(serializedPacket);
+            client.GetStream().Write(buff, 0, buff.Length);
+        }
+
+        void BeginClientListener(TcpClient client)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (client.Connected)
+                    {
+                        try
+                        {
+                            var buffer = new byte[client.ReceiveBufferSize];
+                            client.GetStream().Read(buffer, 0, buffer.Length);
+                            try
+                            {
+                                var packet = JsonConvert.DeserializeObject<Packet>(Encoding.ASCII.GetString(buffer));
+                                //MessageBox.Show("Received Message: " + packet.PacketId);
+
+                                switch (packet.PacketId)
+                                {
+                                    case "authentication":
+                                        var authPacket = (AuthenticationPacket)packet;
+                                        SendAuthenticationResponse(client, "null");
+                                        OnClientConnected.Invoke(authPacket.TeamName, client);
+                                        break;
+                                }
+                                
+                            }
+                            catch
+                            {
+                                //MessageBox.Show("Could not deserialize");
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.Print("Transfer Error: " + e.ToString());
+                            OnClientDisconnect(client);
+                            break;
+                        }
+                    }
+                }
+            }
+            );
+        }
+    }
+}
